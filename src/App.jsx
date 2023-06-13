125e5a48fb9abda228126a49782b0a7e6152b088
Sagul-09 <nihal.evil.royal@gmail.com>
2023-06-13 12:03:39 +0530

    front-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [songs, setSongs] = useState([]);
+  const [newSong, setNewSong] = useState({
+    title: "",
+    artist: "",
+    audioUrl: "",
+    imageUrl: "",
+  });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:1212/api/songs")
+      .then((res) => setSongs(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewSong({ ...newSong, [name]: value });
+  };
+
+  const addSong = () => {
+    axios
+      .post("http://localhost:1212/api/songs", newSong)
+      .then(() => {
+        setSongs([...songs, newSong]);
+        console.log("Song added successfully");
+        setNewSong({
+          title: "",
+          artist: "",
+          audioUrl: "",
+          imageUrl: "",
+        });
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
-    <>
+    <div>
+      <h1>Spicefy Player</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Add song</h2>
+        <label>
+          Title:
+          <input
+            type="text"
+            name="title"
+            value={newSong.title}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Artist:
+          <input
+            type="text"
+            name="artist"
+            value={newSong.artist}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Audio URL:
+          <input
+            type="text"
+            name="audioUrl"
+            value={newSong.audioUrl}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Image URL:
+          <input
+            type="text"
+            name="imageUrl"
+            value={newSong.imageUrl}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button onClick={addSong}>Add Song</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <hr />
+      <div>
+        <h2>Songs</h2>
+        {songs.map((song) => (
+          <div key={song._id}>
+            <img src={song.imageUrl} alt={song.title} />
+            <h3>{song.title}</h3>
+            <p>{song.artist}</p>
+            <audio controls src={song.audioUrl}></audio>
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
